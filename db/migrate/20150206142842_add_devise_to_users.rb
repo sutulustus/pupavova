14571851638813220cc082921fb5a59585756f79
sutulustus <michalisimo@gmail.com>
2015-02-06 15:51:06 +0100

    devise database migration

--- db/migrate/20150206142842_add_devise_to_users.rb
+++ db/migrate/20150206142842_add_devise_to_users.rb
@@ -2,7 +2,6 @@ class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
     change_table(:users) do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -34,6 +33,11 @@ class AddDeviseToUsers < ActiveRecord::Migration
       # Uncomment below if timestamps were not included in your original model.
       # t.timestamps
     end
+
+    remove_column :users, :password
+    remove_column :users, :last_login
+    remove_column :users, :new_password
+    remove_column :users, :new_password_date
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204214637) do
+ActiveRecord::Schema.define(version: 20150206142842) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",              limit: 255,                null: false
-    t.string   "surname",           limit: 255,                null: false
-    t.string   "email",             limit: 255,                null: false
-    t.string   "password",          limit: 255,                null: false
-    t.string   "avatar",            limit: 255
-    t.datetime "last_login"
-    t.string   "new_password",      limit: 255
-    t.datetime "new_password_date"
-    t.boolean  "notifications",     limit: 1,   default: true
-    t.datetime "created_at",                                   null: false
-    t.datetime "updated_at",                                   null: false
+    t.string   "name",                   limit: 255,                null: false
+    t.string   "surname",                limit: 255,                null: false
+    t.string   "email",                  limit: 255,                null: false
+    t.string   "avatar",                 limit: 255
+    t.boolean  "notifications",          limit: 1,   default: true
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.string   "encrypted_password",     limit: 255, default: "",   null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,    null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
