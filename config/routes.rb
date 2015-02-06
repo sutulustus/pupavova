154d4a2584fc885a4517ccfdb43e8545e6973a03
sutulustus <michalisimo@gmail.com>
2015-02-06 16:25:31 +0100

    fixed errors with messages partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+
+  # use devise authenticator
   devise_for :users
   get 'home/index'
 
+  # Mount admin application
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root 'home#index'
+  root to: 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
